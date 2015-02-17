f0ce2b04715d2d8c93d3caceaf6c0be3b47b020d
Yi Sun <seesunmoon@gmail.com>
2015-02-16 22:49:48 -0800

    Initial Commit

---
+++ AdmobiOSSDK7/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdmobiOSSDK7
+//
+//  Created by Yi Sun on 2015-02-16.
+//  Copyright (c) 2015 YS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
